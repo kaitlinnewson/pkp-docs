9c604061500deca63b28743e60478ddffd7f0277
Kaitlin Newson <kaitlin.newson@utoronto.ca>
2021-08-20 13:56:47 -0400

    add basic search example

---
+++ _includes/js/docsearch.js
@@ -0,0 +1,6 @@
+docsearch({ 
+    apiKey: 'fe43f0a02b70dc0208abf4f15cefa7ac', 
+    indexName: 'pkp-sfu', 
+    inputSelector: '#site-search', 
+    debug: false // Set debug to true if you want to inspect the dropdown 
+});
